422eb39e464d75108e4e7098a925b16c52a57321
Carlo Aguirre <carlo_aguirre@outlook.cl>
2022-10-27 13:13:47 -0300

    Busquedas generales added

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,6 +1,8 @@
 import { response } from "express";
 import { isValidObjectId } from "mongoose";
 import { Usuario } from "../models/usuario.js";
+import { Categoria } from "../models/categoria.js";
+import { Producto } from "../models/producto.js";
 
 const coleccionesValidas =[
     'usuarios',
@@ -9,6 +11,8 @@ const coleccionesValidas =[
     'roles'
 ];
 
+
+//metodo para buscar usuarios de tipo '/buscar/usuarios/termino' 
 const buscarUsuarios = async(termino = '', res = response)=>{
 
     //enviaremos en id del usuario como termino de la coleccion 'usuarios'
@@ -17,13 +21,73 @@ const buscarUsuarios = async(termino = '', res = response)=>{
     if(esMongoID){
         const usuario = await Usuario.findById(termino)
 
-        res.status(200).json({
+        return res.status(200).json({
             results: (usuario) ? [usuario] : []
         })
     };
 
+    //Haremos que la bsuqueda sea flexible para buscar usuarios por nombre o correo
+    
+    //primero igualaremos el 'termino' a una expresion regular key sensitive
+    const regex = new RegExp(termino, 'i');
+    
+    const usuarios = await Usuario.find({
+        //Mongo nos permite utilizar expresion como $or y $and para los criterios de busqueda
+        $or: [{nombre: regex}, {correo: regex}],
+        $and: [{estado: true}]
+    });
+    
+    res.json({
+        results: usuarios
+    })
 }
 
+
+//metodo para buscar categorias de tipo '/buscar/categoria/termino' 
+const buscarCategorias = async(termino = '', res = response)=>{
+    const esMongoID = isValidObjectId(termino)
+
+    if(esMongoID){
+        const categoria = await Categoria.findById(termino);
+
+        return res.status(200).json({
+            results: (categoria) ? [categoria] : []
+        })
+    };
+
+    const regex = new RegExp(termino, 'i');
+
+    const categorias = await Categoria.find({nombre: regex, estado: true});
+
+    res.json({
+        results: categorias
+    })
+ 
+};
+
+
+//metodo para buscar productos de tipo '/buscar/productos/termino'
+const buscarProductos = async(termino = '', res = response)=>{
+    const esMongoID = isValidObjectId(termino);
+
+    if(esMongoID){
+        const producto = await Producto.findById(termino)
+                                .populate('categoria', 'nombre') //Hacemos populate para trar el nombre de la categoria tambien
+
+        return res.status(200).json({
+            results: (producto) ? [producto] : []
+        })
+    };
+
+    const regex = new RegExp(termino, 'i')
+
+    const productos = await Producto.find({nombre: regex, estado: true})
+                            .populate('categoria', 'nombre')
+
+    res.status(200).json({
+        results: productos
+    })
+}
 
 
 
@@ -44,9 +108,11 @@ const getBusqueda = (req, res = response)=>{
 
             break;     
         case 'categorias':
+            buscarCategorias(termino, res)
 
             break;
         case 'productos':
+            buscarProductos(termino, res)
 
             break;
         case 'roles':
@@ -58,13 +124,6 @@ const getBusqueda = (req, res = response)=>{
                 msg: 'Este busqueda aun no ha sido desarrollado'
             })
     }
-
-
-    // res.json({
-    //     coleccion,
-    //     termino
-    // });
-
 };
 
 export{
