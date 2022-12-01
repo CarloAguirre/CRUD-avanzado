1282e9cebf5fd655096c0f58d8f261dddc909a53
Carlo Aguirre <carlo_aguirre@outlook.cl>
2022-11-30 23:21:39 -0300

    final fix

--- models/producto.js
+++ models/producto.js
@@ -35,11 +35,9 @@ const ProductoSchema = Schema({
         type: String,
         required: true
     },
-    disponible: {
-        type: Boolean, 
-        default: true},
+    disponible: {type: Boolean, default: true},
     img: {type: String},
-    img2: {type: String},
+    imgDos: {type: String},
     itinerario:{type: String},
     requisitos:{type: String},
     altura:{type: String},
